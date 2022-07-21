e0758a53ab875106e66098c07aaba48ac600ed58
Yuri Bohatyuk <yuri.bohatyuk@a5labs.co>
2022-07-21 14:35:03 +0300

    feat: reformat docker entrypoint sh

--- build/docker-entrypoint.sh
+++ build/docker-entrypoint.sh
@@ -5,9 +5,10 @@ set -e
 host="$(getopt -q -u -l "host:" "h:" "$@" | grep -oP "(?<= --host | -h ).*(?= --.*)" || printf ".")"
 port="$(getopt -q -u -l "port:" "$@" | grep -oP "(?<= --port ).*(?= --.*)" || printf ".")"
 
-while ! cqlsh "$host" "$port" -e 'describe cluster' ; do
-    sleep 3
-    echo "waiting for cassandra"
+echo "Waiting for cassandra to be ready..."
+
+while ! cqlsh "$host" "$port" -e 'describe cluster'; do
+  sleep 3
 done
 
 exec dotnet Cassandra.Migrations.dll "$@"
